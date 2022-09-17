e257ea9f2f679020449d3e2583a04225338735dd
여찬규 <wry5533@gmail.com>
2022-09-17 17:25:51 +0900

    feat: socket 설정

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,17 @@
-alert('test!');
+const socket = io('/');
+const getElementById = (id) => document.getElementById(id) || null;
+
+//* get DOM element
+const helloStrangerElement = getElementById('hello_stranger');
+const chattingBoxElement = getElementById('chatting_box');
+const chatFormElement = getElementById('chat_form');
+
+function helloUser() {
+	const username = prompt('이름을 입력해주세요');
+}
+
+function init() {
+	helloUser();
+}
+
+init();
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import mongoose from 'mongoose';
 import { AppController } from './app.controller';
+import { ChatsModule } from './chats/chats.module';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({ isGlobal: true }),
 		MongooseModule.forRoot(process.env.MONGO_URL),
+		ChatsModule,
 	],
 	controllers: [AppController],
 	providers: [],
